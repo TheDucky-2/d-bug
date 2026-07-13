4159aac720008cbb840c169db1d49553566fd6ba
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-07-13 21:12:09 +0530

    Fixed validator logic and dependencies

--- server/validators/user.py
+++ server/validators/user.py
@@ -1,13 +1,15 @@
-from auth.auth import get_current_user
-from fastapi import Depends, HTTPException
-from config.db import get_db
+from services.Authenticator import Authenticator
+from fastapi import Depends, HTTPException, Request, Response
 from sqlalchemy.orm import Session
 from models.User import User
+from config.db import get_db
 
 
-def validate_user(user_id:int = Depends(get_current_user), db: Session = Depends(get_db)):
+def validate_user(request:Request, response:Response, db:Session = Depends(get_db)):
+
+    user = Authenticator.get_current_user(request=request, response=response, db=db)
     # Validating user
-    user = db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.user_id == user.user_id).first()
 
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
