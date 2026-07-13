b99fc7b083f3bbfcea49aa366b21fcf0c5e48be5
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-07-13 21:14:58 +0530

    Updated imports, models

--- server/auth/auth.py
+++ server/auth/auth.py
@@ -2,7 +2,6 @@ from pwdlib import PasswordHash
 from fastapi import Request, HTTPException, Response
 from logger.logger import create_logger
 from fastapi.security import HTTPBearer
-from utils.jwt_token import verify_refresh_token, generate_access_token, verify_access_token
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -27,57 +26,3 @@ def verify_hash(password:str, hashed_password: str):
     if not verified_hash:
         logger.info("Hash Verification Failed")
         raise HTTPException(status_code=401, detail="Incorrect username or password.")
-
-
-def get_current_user(request:Request, response:Response):
-    
-    access_token = request.cookies.get("access_token") 
-    refresh_token = request.cookies.get("refresh_token")
-
-    decoded_access_token = None
-
-    # if access token exists, fetch user id from decoded token 
-    if access_token:
-        decoded_access_token = verify_access_token(access_token)
-
-    # if access token does not exist, check if refresh token exists
-    if decoded_access_token is None:
-        if not refresh_token:
-            raise HTTPException(status_code=401, detail="Invalid or missing tokens")
-        
-        # if refresh token exists, verify it and decode to get user id
-        decoded_refresh_token = verify_refresh_token(refresh_token)
-
-        if decoded_refresh_token is None:
-            raise HTTPException(status_code=401, detail="Invalid or missing refresh token")
-        
-        user_id = decoded_refresh_token.get("user_id")
-
-        if not user_id:
-            logger.info("Missing user_id")
-            raise HTTPException(status_code=404, detail="Missing credentials")
-        
-        # generating a new access token with the user id
-
-        new_access_token = generate_access_token({"user_id" : user_id})
-
-         # setting the cookie with access token
-
-        response.set_cookie(
-            key="access_token",
-            value=new_access_token,
-            httponly=True,
-            secure= (ENVIRONMENT == "production"),
-            samesite="lax"
-        )
-
-        return user_id 
-    
-    # if missing user_id
-    user_id = decoded_access_token.get("user_id")
-
-    if not user_id:
-        raise HTTPException(status_code=404, detail="Missing user_id")
-    
-    return user_id
-    
--- server/models/Organization.py
+++ server/models/Organization.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy import ForeignKey, DateTime, func, String
+from sqlalchemy import ForeignKey, DateTime, func, Integer
 from constants.enums import OrganizationStatus
 from schemas.project import ProjectResponse
 from typing import List
@@ -18,9 +18,9 @@ class Organization(Base):
     organization_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     organization_name: Mapped[str] = mapped_column()
     organization_logo_url: Mapped[str | None] = mapped_column(nullable=True)
-    organization_members: Mapped[List[str]] = mapped_column(ARRAY(String))
+    organization_members: Mapped[List[int]] = mapped_column(ARRAY(Integer))
     organization_status: Mapped[OrganizationStatus] = mapped_column(default=OrganizationStatus.ACTIVE)
-    organization_owner: Mapped[int | None] = mapped_column(ForeignKey("users.id"), nullable=True)
+    organization_owner: Mapped[int | None] = mapped_column(ForeignKey("users.user_id"), nullable=True)
     organization_projects: Mapped[List["Project"]]  = relationship(back_populates="organization")
 
 
