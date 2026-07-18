a6d370f6952f83232e5365c5f9b45ee91ee2f33b
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-07-18 10:28:06 +0530

    Fixed insert_permissions and added the script to app

--- server/app/main.py
+++ server/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from config.db import get_db
+from config.db import get_db, SessionLocal
 from routes.auth_router import auth_router
 from routes.org_router import org_router
 from routes.project_router import project_router
+from auth.authorization.insert_permissions import insert_permissions
 from config.db import Base, engine
 import models
 from dotenv import load_dotenv
+from logger.logger import create_logger
 import os
 
 Base.metadata.create_all(engine)
@@ -15,6 +17,16 @@ Base.metadata.create_all(engine)
 app = FastAPI()
 
 allowed_origins = [os.environ["FRONTEND_URL"], "http://127.0.0.1/5173"]
+logger = create_logger(__name__.split(".")[0])
+
+try: 
+    db = SessionLocal()
+    logger.info("Inserting permissions into the database")
+    insert_permissions(db)
+    
+except Exception as error:
+    logger.info("Received error while inserting permissions into the database")
+    raise error
 
 app.include_router(auth_router)
 app.include_router(org_router)
