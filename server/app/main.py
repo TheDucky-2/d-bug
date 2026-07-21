8c08d8565a1160c8e41a5e24ac784d123a151716
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-07-21 21:33:32 +0530

    Updated main.py

--- server/app/main.py
+++ server/app/main.py
@@ -5,49 +5,39 @@ from config.db import get_db, SessionLocal
 from routes.auth_router import auth_router
 from routes.org_router import org_router
 from routes.project_router import project_router
+from routes.email_router import email_router
 from auth.authorization.insert_permissions import insert_permissions
 from config.db import Base, engine
-<<<<<<< HEAD
-import models
-from dotenv import load_dotenv
 from logger.logger import create_logger
-=======
 from auth.authorization.insert_permissions import insert_permissions
->>>>>>> main
 import os
+
+FRONTEND_URL=os.environ["FRONTEND_URL"]
+allowed_origins = [FRONTEND_URL, "http://127.0.0.1:5173"]
+logger = create_logger(__name__.split(".")[1])
 
 Base.metadata.create_all(engine)
 
-db = SessionLocal()
+app = FastAPI()
 
-try:
-    db = SessionLocal()
+db = SessionLocal()
+try: 
     insert_permissions(db)
 
 except Exception as error:
+    logger.info("Received error while inserting permissions into the database")
     raise error
 
 finally:
     db.close()
 
 
-app = FastAPI()
-
-allowed_origins = [os.environ["FRONTEND_URL"], "http://127.0.0.1:5173"]
-logger = create_logger(__name__.split(".")[0])
-
-try: 
-    db = SessionLocal()
-    logger.info("Inserting permissions into the database")
-    insert_permissions(db)
-    
-except Exception as error:
-    logger.info("Received error while inserting permissions into the database")
-    raise error
 
 app.include_router(auth_router)
 app.include_router(org_router)
 app.include_router(project_router)
+app.include_router(email_router)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
