077e024c2cf7d1353ebe1b47183001d9c06a953b
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-08-06 21:44:47 +0530

    Added empty states

--- client/src/pages/Organization/EmptyInbox.tsx
+++ client/src/pages/Organization/EmptyInbox.tsx
@@ -12,14 +12,13 @@ import {
 const EmptyInbox = () => {
   return (
   <>
-  <style>
-  {`
-  @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&family=Outfit:wght@100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
-
-  * {
-      font-family: "Manrope", sans-serif;
-  }
-`}</style>
+     <style>{`
+      @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&family=Outfit:wght@100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
+  
+      * {
+          font-family: "Inter";
+      }
+    `}</style>
     <div className="h-screen justify-center flex">
     <Empty >
       <EmptyHeader>
