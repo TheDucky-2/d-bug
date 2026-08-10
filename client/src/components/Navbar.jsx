1eab5ecef35cbd18981ad4d49873029b9f7557f3
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-08-10 20:39:04 +0530

    Updated CSS for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,15 +14,8 @@ const Navbar = () => {
 
   return (
     <>
-    <style>{`
-            @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&family=Outfit:wght@100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
-        
-            * {
-                font-family: "Inter";
-            }
-    `}</style>
-   
-      <div className={`shadow-md sticky top-0 z-50 justify-between items-center p-6 flex h-16 w-full m-0 ${isDark ? "bg-zinc-900 text-white/60 border-b border-zinc-100/10" : "bg-zinc-100 text-black border-b border-gray-300"} `}>
+      <div className={`shadow-md sticky top-0 z-50 justify-between items-center p-6 flex h-16 w-full m-0
+         ${isDark ? "bg-zinc-900 text-white/60 border-b border-zinc-100/10" : "bg-zinc-100 text-black border-b border-gray-300"} `}>
         
         {/** Logo container */}
         <div>
@@ -33,40 +26,57 @@ const Navbar = () => {
 
         {/** link container */}
         <div className={`flex gap-12 justify-between`}>
-          <Link className={`text-sm hover:text-blue-400`} to="/"> Home</Link>
-          <Link className="text-sm  hover:text-blue-400" to="/features"> Features </Link>
-          <Link className="text-sm  hover:text-blue-400" to="/pricing"> Pricing</Link>
-          <Link className="text-sm  hover:text-blue-400" to="/pricing"> Docs </Link>
+          <Link className={`text-base font-semibold hover:text-blue-500 dark:hover:text-blue-400`} to="/"> Home</Link>
+          <Link className="text-base  font-semibold hover:text-blue-500 dark:hover:text-blue-400" to="/features"> Features </Link>
+          <Link className="text-base font-semibold hover:text-blue-500 dark:hover:text-blue-400" to="/pricing"> Pricing</Link>
+          <Link className="text-base font-semibold hover:text-blue-500 dark:hover:text-blue-400" to="/pricing"> Docs </Link>
 
         </div>
 
+
         <div className="flex justify-between items-center gap-10">
-          {/** login button container */}
-          {isLoggedIn
-          ?
-          (<button onClick={()=> navigate("/dashboard")}
-          className={`cursor-pointer rounded text-l px-3.5 py-1.5 m-2 transition font-medium ${isDark ? "bg-green-600 hover:bg-green-700 text-white/90" : "bg-green-400 hover:bg-green-500 text-zinc-800"}`}>
-          Go To Dashboard
-          </button>  )
+          
+          {/** Button Container */}
 
-          :(
-          
-          <button onClick={()=> navigate("/auth/sign-in")}
-          className={`cursor-pointer rounded-sm text-sm px-4 py-2 m-0 transition-all font-medium flex items-center gap-2 border
-          ${isDark  ? " hover:bg-zinc-300/20 text-white border-white/40" : " hover:bg-zinc-700/20 text-black border-black/40"}`}>
-          
-          Log in
-          </button>
+          <div className="flex items-center gap-2">
+
+            {/**Sign up button */}
+            <button onClick={()=> navigate("/auth/sign-up")}
+            className={`cursor-pointer text-sm px-4 py-2 m-0 transition-all font-bold flex items-center gap-2 
+             dark:text-white dark:border-white/40 text-black hover:text-blue-500 dark:hover:text-blue-400 border-black/40`}>
+            
+            Sign up
+            </button>
+
+            {/** Login button  */}
+            {isLoggedIn
+            ?
+            (<button onClick={()=> navigate("/dashboard")}
+            className={`cursor-pointer rounded text-l px-3.5 py-1.5 m-2 transition font-medium ${isDark ? "bg-green-600 hover:bg-green-700 text-white/90" : "bg-green-400 hover:bg-green-500 text-zinc-800"}`}>
+            Go To Dashboard
+            </button>  )
+
+            :(
+            
+            <button onClick={()=> navigate("/auth/sign-in")}
+            className={`cursor-pointer rounded-full text-sm px-4 py-2 m-0 transition-all font-bold flex items-center gap-2 border
+             dark:hover:bg-zinc-300  dark:border-white/40 
+             hover:bg-zinc-700 bg-black dark:bg-white dark:text-black text-white border-black/40`}>
+            Log in
+            </button>
         )} 
+        </div>
 
+        
+        {/** Toggle Theme Button */}
         <Tooltip>
           <TooltipTrigger asChild>
           {/** theme changing container */}
-          <button onClick={toggleTheme} className={`cursor-pointer transition hover:text-blue-400`} >
+          <button onClick={toggleTheme} className={`cursor-pointer transition hover:text-blue-500 dark:hover:text-blue-400`} >
           
           {isDark
-          ? <Sun className="h-5 w-5 text-zinc-700 dark:text-zinc-400  dark:hover:text-blue-400 transition-colors" />  
-          : <Moon className="h-5 w-5 text-zinc-700 dark:text-zinc-400 hover:text-blue-400 transition-colors" />}
+          ? <Sun className="h-5 w-5 text-zinc-700 dark:text-zinc-300  hover:text-blue-500 dark:hover:text-blue-400 transition-colors" />  
+          : <Moon className="h-5 w-5 text-zinc-700 dark:text-zinc-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors" />}
           </button>
           </TooltipTrigger>
           <TooltipContent className={`rounded-sm dark:bg-zinc-100 bg-zinc-800 font-medium`}>
