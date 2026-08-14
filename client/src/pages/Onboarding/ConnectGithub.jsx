0fec47a765fa8690b085966596ecde950a238c36
kshitij Sharma <kshitijsharma1960@gmail.com>
2026-08-14 20:49:24 +0530

    Updated onboarding flow

--- client/src/pages/Onboarding/ConnectGithub.jsx
+++ client/src/pages/Onboarding/ConnectGithub.jsx
@@ -14,8 +14,17 @@ const ConnectGithub = () => {
   const handleSkip = () => {
     navigate("/organization")
   }
+     <style>{`
+            @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&family=Outfit:wght@100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap');
+        
+            * {
+                font-family: "Inter";
+            }
+    `}
+    </style>
 
   return (
+    <>
     <div className="flex flex-col items-center gap-8 sm:gap-10">
 
       <div className="text-center">
@@ -50,6 +59,7 @@ const ConnectGithub = () => {
       </p>
 
     </div>
+    </>
   )
 }
 
